422f9713b3ce2f46b070738211811e00b4ce3a19
Seongkyun-Yu <ysungkyun@gmail.com>
2020-06-27 22:21:02 +0900

    feat: Make Booking Template

---
+++ src/Components/Templates/Booking.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Organisms/Header";
+import Footer from "../Organisms/Footer";
+
+const MyMegaBox = () => {
+  return (
+    <div>
+      <Header />
+      부킹 서브라우터 적용해야함
+      <Footer />
+    </div>
+  );
+};
+
+export default MyMegaBox;
