79c030c4e691b927d5a89819e3454ac467183f87
Seongkyun-Yu <ysungkyun@gmail.com>
2020-06-27 21:01:07 +0900

    feat: Make Footer Page

---
+++ src/Components/Organisms/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>Header</footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Organisms/Header.js
+++ src/Components/Organisms/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <header>Header</header>
     </div>
   );
 };
+
+export default Header;
