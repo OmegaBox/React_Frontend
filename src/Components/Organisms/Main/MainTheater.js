1bd642a7451733a0a21db9f5be86a4727d393703
Seongkyun-Yu <ysungkyun@gmail.com>
2020-06-27 22:18:03 +0900

    feat: Make Main Template

--- src/Components/Organisms/Main/MainTheater.js
+++ src/Components/Organisms/Main/MainTheater.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const MainTheather = () => {
+const MainTheater = () => {
   return <div>메인 영화관 리스트</div>;
 };
 
-export default MainTheather;
+export default MainTheater;
--- src/Components/Templates/MyMegaBox.js
+++ src/Components/Templates/MyMegaBox.js
@@ -1,15 +1,17 @@
 import React from "react";
-import Header from "../Organisms/Header"
-import Footer from "../Organisms/Footer"
-import Snb from "../Organisms/MyPage/Snb"
-import BookingHistoryWrap from "../Organisms/MyPage/BookingHistoryWrap"
+import Header from "../Organisms/Header";
+import Footer from "../Organisms/Footer";
+import Snb from "../Organisms/MyPage/Snb";
+import BookingHistoryWrap from "../Organisms/MyPage/BookingHistoryWrap";
 
 const MyMegaBox = () => {
   return (
-    <Header />
-    <Snb />
-    <BookingHistoryWrap />
-    <Footer />
+    <div>
+      <Header />
+      <Snb />
+      <BookingHistoryWrap />
+      <Footer />
+    </div>
   );
 };
 
