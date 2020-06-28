c13c19e625cd97f7528004aa444003184ec13374
Seongkyun-Yu <ysungkyun@gmail.com>
2020-06-29 00:11:18 +0900

    feat: fix some bugs

--- src/Components/Molecules/CarouselCalendar.js
+++ src/Components/Molecules/CarouselCalendar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 
 import { getDateRangeData } from "../../Utils/ultil";
@@ -7,14 +7,20 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./style/CarouselCalendar.scss";
 
-const test = (value, e) => {
-  console.log(value);
-  console.log(e.currentTarget);
-};
-
 const dateArray = getDateRangeData(Date.now(), "2020-07-20");
 
 const CarouselCalendar = () => {
+  const [clickCalIndex, setClickCalIndex] = useState(0);
+
+  const test = (value, e) => {
+    console.log(value);
+    console.log(e.currentTarget.id);
+    console.log(e);
+    setClickCalIndex(e.currentTarget.id);
+  };
+
+  console.log("리페인팅", clickCalIndex);
+
   const settings = {
     dots: false,
     infinite: false,
@@ -22,6 +28,7 @@ const CarouselCalendar = () => {
     slidesToShow: 14,
     slidesToScroll: 1,
     arrows: true,
+    focusOnSelect: true,
     // nextArrow: <NextArrow />,
     // prevArrow: <PrevArrow />,
   };
@@ -30,9 +37,14 @@ const CarouselCalendar = () => {
     <div className="container" style={{ padding: "40px", width: "1100px" }}>
       <Slider {...settings}>
         {dateArray.map((date, i) => {
+          if (clickCalIndex === +i) console.log("일치!", i);
           return (
-            <div className="carouselCal" onClick={(e, i) => test(date.day, e)}>
-              <span>
+            <div
+              className="carouselCal"
+              id={i}
+              onClick={(e) => test(date.day, e)}
+            >
+              <span className={+clickCalIndex === +i ? "dododo" : "no"}>
                 {date.day} {date.dayOfWeek}
               </span>
             </div>
