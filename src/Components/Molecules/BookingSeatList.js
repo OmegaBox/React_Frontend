b131ed40cf495f002d52bb22cc10e3494219a4f0
Mang-developer <chiwon618@gmail.com>
2020-06-29 15:26:56 +0900

    Docs: Made SeatDefault form
    좌석 리스트 뷰 영역에 기본 형식을 제작

--- src/Components/Molecules/BookingSeatList.js
+++ src/Components/Molecules/BookingSeatList.js
@@ -1,7 +1,37 @@
 import React from "react";
 
+const seatInfo = {
+  maxSeat: 17,
+  row: 15,
+};
+
+const rowNames = new Array(seatInfo.row)
+  .fill(0)
+  .map((v, i) => String.fromCharCode(65 + i));
+
+const SeatNums = new Array(seatInfo.maxSeat).fill(0).map((v, i) => i + 1);
+
 const BookingSeatList = () => {
-  return <div>좌석 리스트</div>;
+  return (
+    <div id="bookingSeatList">
+      <ul id="listRowName">
+        {rowNames.map((v) => (
+          <li key={`rowName ${v}`}>{v}</li>
+        ))}
+      </ul>
+      <ul id="listRow">
+        {rowNames.map((row) => (
+          <li key={`row ${row}`}>
+            <ul className="listSeat">
+              {SeatNums.map((num) => (
+                <li key={`${row}${num}`}>{num}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default BookingSeatList;
