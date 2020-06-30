bea7e9679f81e9eaca2e05ca1d68823f4e02d5b0
hodeng28 <ghepd28@gmail.com>
2020-06-30 18:20:49 +0900

    feat: footer init

--- src/Components/Organisms/Footer.js
+++ src/Components/Organisms/Footer.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./Footer.scss";
-import logo from "../../images/omegabox_logo.jpg"
+import logo from "../../images/omegaGray.png"
 
 const Footer = () => {
   return (
     <div>
       <footer className="footerLayout">
-        <div>
+        <div className="footerWrap">
           <img
             className="footerLogo"
             alt="omegaboxLogo"
             src={logo}
           />
+          <p>
+            FRONT-END 유성균 김규리 정호영 송치원
+          </p>
+          <p>
+            BACK-END 권효진 신동현
+          </p>
+          <p>Copyright since &copy; OMEGABOX CORPORATION ALL RIGHTS RESERVED.</p>
         </div>
       </footer>
     </div>
