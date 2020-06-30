571496b3d301aa8b699a464cda5d761289b08ab5
hodeng28 <ghepd28@gmail.com>
2020-06-30 20:04:57 +0900

    feat: make footer

--- src/Components/Organisms/Footer.js
+++ src/Components/Organisms/Footer.js
@@ -12,13 +12,38 @@ const Footer = () => {
             alt="omegaboxLogo"
             src={logo}
           />
-          <p>
-            FRONT-END 유성균 김규리 정호영 송치원
-          </p>
-          <p>
-            BACK-END 권효진 신동현
-          </p>
-          <p>Copyright since &copy; OMEGABOX CORPORATION ALL RIGHTS RESERVED.</p>
+          <ul className="madeIn">
+            <li>FRONT-END. &nbsp; 유성균 김규리 정호영 송치원 &emsp; BACK-END.&nbsp; 권효진 신동현</li>
+            <li>GITHUB.&nbsp; <a href="https://github.com/OmegaBox"
+            >https://github.com/OmegaBox</a></li>
+            <li>Copyright &copy; 2020 OMEGABOX CORPORATION ALL RIGHTS RESERVED.</li>
+          </ul>
+          <ul className="snsIcons">
+            <li className="snsTwitter">
+              <img
+                alt="트위터 아이콘"
+                src="https://img.megabox.co.kr/static/pc/images/common/ico/ico-twitter.png"
+              />
+            </li>
+            <li className="snsFacebook">
+              <img
+                alt="페이스북 아이콘"
+                src="https://img.megabox.co.kr/static/pc/images/common/ico/ico-facebook.png"
+              />
+            </li>
+            <li className="snsInstagram">
+              <img
+                alt="인스타그램 아이콘"
+                src="https://img.megabox.co.kr/static/pc/images/common/ico/ico-instagram.png"
+              />
+            </li>
+            <li className="snsApple">
+              <img
+                alt="애플스토어 아이콘"
+                src="https://img.megabox.co.kr/static/pc/images/common/ico/ico-appstore.png"
+              />
+            </li>
+          </ul>
         </div>
       </footer>
     </div>
