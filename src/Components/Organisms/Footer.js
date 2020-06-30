b0f211d8e2565fb5b25c6d319275f9cfa9d60765
hodeng28 <ghepd28@gmail.com>
2020-06-30 17:35:19 +0900

    fix: header layout

--- src/Components/Organisms/Footer.js
+++ src/Components/Organisms/Footer.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Footer.scss";
+import logo from "../../images/omegabox_logo.jpg"
 
 const Footer = () => {
   return (
     <div>
-      <footer className="footerLayout">footer</footer>
+      <footer className="footerLayout">
+        <div>
+          <img
+            className="footerLogo"
+            alt="omegaboxLogo"
+            src={logo}
+          />
+        </div>
+      </footer>
     </div>
   );
 };
