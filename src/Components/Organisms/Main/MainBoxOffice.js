37224e333a143235042393cdec8ca19065ffb906
hodeng28 <ghepd28@gmail.com>
2020-07-01 01:50:19 +0900

    feat: make boxOffice component

--- src/Components/Organisms/Main/MainBoxOffice.js
+++ src/Components/Organisms/Main/MainBoxOffice.js
@@ -7,7 +7,10 @@ const MainBoxOffice = () => {
   return (
     <div className="mainBoxOfficeLayout">
       <div className="mainBoxOffice">
-        <h2>박스오피스</h2>
+        <div>
+          <h2 className="boxOfficeHeader">박스오피스</h2>
+          <a href="#"><span>더 많은 영화보기 +</span></a>
+        </div>
         <div className="mainMovieList">
           <ul class="mainMoviesWrap">
             <li>
@@ -18,11 +21,58 @@ const MainBoxOffice = () => {
               />
               <div className="boxOfficeBtnWrap">
                 <button className={["boxOfficeFavoriteBtn", "btn", "outLine", "lightGray", "large"].join(" ")}>
-                  <span class="icon favorite">
-                    <span class="">1000</span>
-                  </span>
+                  <span className="icon Mainfavorite"></span>
+                  <span className="boxOfficeFavoriteScore">1000</span>
                 </button>
-                <button className={["boxOfficeBookingBtn", "btn", "fill", "subDark", "large"].join(" ")}>
+                <button className={["boxOfficeBookingBtn", "btn", "fill", "subLight", "large"].join(" ")}>
+                  예매
+              </button>
+              </div>
+            </li>
+            <li>
+              <img
+                class="boxOfficeMoviePoster"
+                alt="살아있다"
+                src="https://img.megabox.co.kr/SharedImg/2020/06/02/xIBdAOS5lJNBe1CBXovcV1WYE9Q6DWPV_420.jpg"
+              />
+              <div className="boxOfficeBtnWrap">
+                <button className={["boxOfficeFavoriteBtn", "btn", "outLine", "lightGray", "large"].join(" ")}>
+                  <span class="icon Mainfavorite"></span>
+                  <span class="boxOfficeFavoriteScore">1000</span>
+                </button>
+                <button className={["boxOfficeBookingBtn", "btn", "fill", "subLight", "large"].join(" ")}>
+                  예매
+              </button>
+              </div>
+            </li>
+            <li>
+              <img
+                class="boxOfficeMoviePoster"
+                alt="살아있다"
+                src="https://img.megabox.co.kr/SharedImg/2020/06/02/xIBdAOS5lJNBe1CBXovcV1WYE9Q6DWPV_420.jpg"
+              />
+              <div className="boxOfficeBtnWrap">
+                <button className={["boxOfficeFavoriteBtn", "btn", "outLine", "lightGray", "large"].join(" ")}>
+                  <span class="icon Mainfavorite"></span>
+                  <span class="boxOfficeFavoriteScore">1000</span>
+                </button>
+                <button className={["boxOfficeBookingBtn", "btn", "fill", "subLight", "large"].join(" ")}>
+                  예매
+              </button>
+              </div>
+            </li>
+            <li>
+              <img
+                class="boxOfficeMoviePoster"
+                alt="살아있다"
+                src="https://img.megabox.co.kr/SharedImg/2020/06/02/xIBdAOS5lJNBe1CBXovcV1WYE9Q6DWPV_420.jpg"
+              />
+              <div className="boxOfficeBtnWrap">
+                <button className={["boxOfficeFavoriteBtn", "btn", "outLine", "lightGray", "large"].join(" ")}>
+                  <span class="icon Mainfavorite"></span>
+                  <span class="boxOfficeFavoriteScore">1000</span>
+                </button>
+                <button className={["boxOfficeBookingBtn", "btn", "fill", "subLight", "large"].join(" ")}>
                   예매
               </button>
               </div>
