412304720faebfa629fe3438e799452f07b1d76a
hodeng28 <ghepd28@gmail.com>
2020-07-02 20:03:05 +0900

    feat: make boxOffice Reducer

--- src/Components/Organisms/Main/MainBoxOffice.js
+++ src/Components/Organisms/Main/MainBoxOffice.js
@@ -1,32 +1,27 @@
-import React, { useState, useReducer, useEffect } from "react";
+import React, { useEffect } from "react";
 import { movieApi } from "../../../Api/api";
 import "./style/MainBoxOffice.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
-const initMovie = async () => {
-  const test = await movieApi.getMovies();
-  console.log(test);
-};
 
-const MainBoxOffice = ({ }) => {
-
-  useSelector((state) => state.initMovie)
+const MainBoxOffice = () => {
+  useSelector((state) => state.initMovie);
   const dispatch = useDispatch();
-
   useEffect(() => {
-    const mainBoxOfficeMovies = async () => {
+    const initMovie = async () => {
       try {
         dispatch({ type: "LOADING" });
-        let moviesData = await movies.getMainBoxOffice();
-        if (moviesData.status === 200) {
-          dispatch({ type: "SUCCESS", data: moviesData.data.results });
+        const test = await movieApi.getMovies();
+        console.log(test);
+        if (test.status === 200) {
+          dispatch({ type: "SUCCESS", data: test.data.results });
         } else {
           dispatch({
             type: "ERROR",
             error: {
               state: true,
-              message: moviesData.statusText,
+              message: test.statusText,
             },
           });
         }
@@ -40,85 +35,79 @@ const MainBoxOffice = ({ }) => {
         });
       }
     };
-
-    mainBoxOfficeMovies();;
+    initMovie();
   }, []);
 
-  const MainBoxOffice = () => {
-    const initialState = {};
-
-    const mainBoxOfficeReducer = (state = initialState, action) => { };
-    return (
-      <div className="mainBoxOfficeLayout">
-        <div className="mainBoxOffice">
-          <div className="mainBoxOfficeHeader">
-            <h2>박스오피스</h2>
-            <Link to="/">
-              <span className="boxOfficeMoreMovie">더 많은 영화보기 +</span>
-            </Link>
-          </div>
-          <div className="mainMovieList">
-            <ul class="mainMoviesWrap">
-              <li>
-                <img
-                  class="boxOfficeMoviePoster"
-                  alt="살아있다"
-                  src="https://img.megabox.co.kr/SharedImg/2020/06/02/xIBdAOS5lJNBe1CBXovcV1WYE9Q6DWPV_420.jpg"
-                />
-                <div className="boxOfficeBtnWrap">
-                  <button
-                    className={[
-                      "boxOfficeFavoriteBtn",
-                      "btn",
-                      "outLine",
-                      "lightGray",
-                      "small",
-                    ].join(" ")}
-                  >
-                    <span className="icon favoriteOutLine"></span>
-                    <span className="boxOfficeFavoriteScore">1000</span>
-                  </button>
-                  <button
-                    className={[
-                      "boxOfficeBookingBtn",
-                      "btn",
-                      "fill",
-                      "subLight",
-                      "small",
-                    ].join(" ")}
-                  >
-                    예매
-                </button>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <ul className="boxOfficeSubBarWrap">
-            <li className="boxOfficeSearchBarWrap">
-              <input
-                type="text"
-                className="boxOfficeSearchBar"
-                placeholder="영화명을 입력해주세요."
+  return (
+    <div className="mainBoxOfficeLayout">
+      <div className="mainBoxOffice">
+        <div className="mainBoxOfficeHeader">
+          <h2>박스오피스</h2>
+          <Link to="/">
+            <span className="boxOfficeMoreMovie">더 많은 영화보기 +</span>
+          </Link>
+        </div>
+        <div className="mainMovieList">
+          <ul className="mainMoviesWrap">
+            <li>
+              <img
+                className="boxOfficeMoviePoster"
+                alt="영화이미지"
+                src="https://img.megabox.co.kr/SharedImg/2020/06/02/xIBdAOS5lJNBe1CBXovcV1WYE9Q6DWPV_420.jpg"
               />
-              <button type="button" className="iconSearchBtn"></button>
-            </li>
-            <li>
-              <span className="iconSchedule" />
-              <span className="boxOfficeSearchBarText">상영시간표</span>
-            </li>
-            <li>
-              <span className="iconBoxOffice" />
-              <span className="boxOfficeSearchBarText">박스오피스</span>
-            </li>
-            <li>
-              <span className="iconBoxOfficeBooking" />
-              <span className="boxOfficeSearchBarText">빠른예매</span>
+              <div className="boxOfficeBtnWrap">
+                <button
+                  className={[
+                    "boxOfficeFavoriteBtn",
+                    "btn",
+                    "outLine",
+                    "lightGray",
+                    "small",
+                  ].join(" ")}
+                >
+                  <span className="icon favoriteOutLine"></span>
+                  <span className="boxOfficeFavoriteScore">1000</span>
+                </button>
+                <button
+                  className={[
+                    "boxOfficeBookingBtn",
+                    "btn",
+                    "fill",
+                    "subLight",
+                    "small",
+                  ].join(" ")}
+                >
+                  예매
+                </button>
+              </div>
             </li>
           </ul>
         </div>
+        <ul className="boxOfficeSubBarWrap">
+          <li className="boxOfficeSearchBarWrap">
+            <input
+              type="text"
+              className="boxOfficeSearchBar"
+              placeholder="영화명을 입력해주세요."
+            />
+            <button type="button" className="iconSearchBtn"></button>
+          </li>
+          <li>
+            <span className="iconSchedule" />
+            <span className="boxOfficeSearchBarText">상영시간표</span>
+          </li>
+          <li>
+            <span className="iconBoxOffice" />
+            <span className="boxOfficeSearchBarText">박스오피스</span>
+          </li>
+          <li>
+            <span className="iconBoxOfficeBooking" />
+            <span className="boxOfficeSearchBarText">빠른예매</span>
+          </li>
+        </ul>
       </div>
-    );
-  };
+    </div>
+  );
 };
 
 export default MainBoxOffice;
