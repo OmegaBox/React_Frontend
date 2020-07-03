f2ee8dec9508c3cdad6e6a872cce628dd1e56d6b
hodeng28 <ghepd28@gmail.com>
2020-07-03 15:06:38 +0900

    fix: api

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
 export const movieApi = {
-  getMovies: async (id) => {
-    const res = await axios.get("movies/");
-    return {
-      status: res.status,
-      statusText: res.statusText,
-      data: res.data,
-    };
-  },
+  getMovies: async (id) => axios.get("movies/")
 };
