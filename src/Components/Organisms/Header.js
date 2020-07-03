b0a660dba9e52e0cb1edc19121d555ea46129cb6
hodeng28 <ghepd28@gmail.com>
2020-07-03 20:49:21 +0900

    fix: movieReducer.js

--- src/Components/Organisms/Header.js
+++ src/Components/Organisms/Header.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/heading-has-content */
 import React from "react";
 import "./style/Header.scss";
 import logo from "../../images/omegaWhite.png"
