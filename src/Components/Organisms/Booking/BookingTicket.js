86cafd1c55893c50888f838cb5c5421bdd3df0bf
Mang-developer <chiwon618@gmail.com>
2020-07-06 00:22:42 +0900

    Style: Made SCSS of BookingTicket
    티켓 이미지 출력을 위한 SCSS 작성

--- src/Components/Organisms/Booking/BookingTicket.js
+++ src/Components/Organisms/Booking/BookingTicket.js
@@ -1,9 +1,12 @@
 import React from "react";
 import BookingNotice from "../../Molecules/BookingNotice";
 
+//booking/payment-successcomplete
+
 const BookingTicket = () => {
   return (
     <div>
+      <h2>예매완료</h2>
       <button className={["btn", "main", "outLine"].join(" ")}>
         교환권출력
       </button>
