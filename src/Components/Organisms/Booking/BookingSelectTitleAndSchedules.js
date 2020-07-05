418574993086a260a08dd3f9f16ecab2c7c3bff2
Seongkyun-Yu <ysungkyun@gmail.com>
2020-07-05 16:53:50 +0900

    feat: add some styles

--- src/Components/Organisms/Booking/BookingSelectTitleAndSchedules.js
+++ src/Components/Organisms/Booking/BookingSelectTitleAndSchedules.js
@@ -1,17 +1,24 @@
 import React from "react";
+import CarouselCalendar from "../../Molecules/CarouselCalendar";
+import Calendar from "../../Molecules/Calendar";
 import BookingMovieList from "../../Molecules/BookingMovieList";
 import BookingMovieSchedules from "../../Molecules/BookingMovieSchedules";
+import BookingFastTitle from "../../Atoms/BookingFastTitle";
+import BookingScheduleInfo from "../../Molecules/BookingScheduleInfo";
 
 import "./style/BookingSelectMovie.scss";
-import BookingFastTitle from "../../Atoms/BookingFastTitle";
-
 const BookingSelectTimeAndTheater = () => {
   return (
     <div className="bookingSelectMovieContainer">
       <BookingFastTitle />
+      <div className="calendarsContainer">
+        <CarouselCalendar />
+        <Calendar />
+      </div>
       <div className="bookingInfoLists">
         <BookingMovieList />
         <BookingMovieSchedules />
+        <BookingScheduleInfo />
       </div>
     </div>
   );
