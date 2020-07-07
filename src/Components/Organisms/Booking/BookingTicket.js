630e9c1b85ff96fd39459c2d691a659b468eca38
hyacinta <kimkyuree84@gmail.com>
2020-07-07 18:23:24 +0900

    feat: ticketpage link 연결페이지 링크

--- src/Components/Organisms/Booking/BookingTicket.js
+++ src/Components/Organisms/Booking/BookingTicket.js
@@ -1,16 +1,22 @@
 import React from "react";
+import "./style/BookingTicket.scss";
 import BookingNotice from "../../Molecules/BookingNotice";
 
 //booking/payment-successcomplete
 
 const BookingTicket = () => {
   return (
-    <div>
-      <h2>예매완료</h2>
-      <button className={["btn", "main", "outLine"].join(" ")}>
-        교환권출력
-      </button>
-      <button className={["btn", "main", "fill"].join(" ")}>예매 내역</button>
+    <div className="ticketWrap">
+      <h2 className="pageTitle">예매완료</h2>
+      <div className="ticket">
+        <img src="https://img.megabox.co.kr/static/pc/images/reserve/bg-reserve-finish-top.png" />
+      </div>
+      <div className="btnWrap">
+        <button className={["btn", "main", "outLine"].join(" ")}>
+          교환권출력
+        </button>
+        <button className={["btn", "main", "fill"].join(" ")}>예매 내역</button>
+      </div>
       <BookingNotice />
     </div>
   );
