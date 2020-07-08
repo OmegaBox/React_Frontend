1fa75ef1f4131a9cf6a39ce40ab2edb3608d6ca5
hyacinta <kimkyuree84@gmail.com>
2020-07-08 16:54:30 +0900

    feat: eventPage Layout 이벤트 페이지 레이아웃 누락 페이지 추가

--- src/Router/MainRouter.js
+++ src/Router/MainRouter.js
@@ -3,11 +3,12 @@ import { Switch, Route } from "react-router-dom";
 import MainPage from "../Components/Pages/MainPage";
 import BookingPage from "../Components/Pages/BookingPage";
 import MyPage from "../Components/Pages/MyPage";
+import EventPage from "../Components/Pages/EventPage";
 import LoginAndSignUpPage from "../Components/Pages/LoginAndSignUpPage";
 import NotFoundPage from "../Components/Pages/NotFoundPage";
 import ComponentSample from "../ComponentSample";
 import MovieDetailPage from "../Components/Pages/MovieDetailPage";
-import WholeMovieListPage from "../Components/Pages/WholeMovieListPage"
+import WholeMovieListPage from "../Components/Pages/WholeMovieListPage";
 
 const MainRouter = () => {
   return (
@@ -17,6 +18,7 @@ const MainRouter = () => {
       <Route path="/movieDetail" component={MovieDetailPage} />
       <Route path="/moviesList" component={WholeMovieListPage} />
       <Route path="/mypage" component={MyPage} />
+      <Route path="/event" component={EventPage} />
       <Route path={["/login", "/signup"]} component={LoginAndSignUpPage} />
       <Route path="/stylesample" component={ComponentSample} />
       <Route render={NotFoundPage} />
