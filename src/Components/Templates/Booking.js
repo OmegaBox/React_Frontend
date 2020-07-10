eb2638005c70f426048304dad30ebd442afeb437
Seongkyun-Yu <ysungkyun@gmail.com>
2020-07-10 12:48:49 +0900

    feat: Make isLoginCheck popup & check

--- src/Components/Templates/Booking.js
+++ src/Components/Templates/Booking.js
@@ -3,8 +3,12 @@ import Header from "../Organisms/Header";
 import Footer from "../Organisms/Footer";
 import NavSample from "../Organisms/NavSample";
 import SubBookingRouter from "../../Router/SubBookingRouter";
+import { useSelector, useDispatch } from "react-redux";
+import PopupNotice from "../Molecules/PopupNotice";
+import ModalPortal from "../../Modules/ModalPortal";
+import { setOneBtn } from "../../Reducer/modalReducer";
 
-const Booking = () => {
+const Booking = ({ history }) => {
   return (
     <div>
       <Header />
