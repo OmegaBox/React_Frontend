88e8260f4b4337e235a3cb68e286c21919d1b841
Seongkyun-Yu <ysungkyun@gmail.com>
2020-07-10 13:00:53 +0900

    fix: fix some style

--- src/Components/Templates/Booking.js
+++ src/Components/Templates/Booking.js
@@ -3,10 +3,6 @@ import Header from "../Organisms/Header";
 import Footer from "../Organisms/Footer";
 import NavSample from "../Organisms/NavSample";
 import SubBookingRouter from "../../Router/SubBookingRouter";
-import { useSelector, useDispatch } from "react-redux";
-import PopupNotice from "../Molecules/PopupNotice";
-import ModalPortal from "../../Modules/ModalPortal";
-import { setOneBtn } from "../../Reducer/modalReducer";
 
 const Booking = ({ history }) => {
   return (
