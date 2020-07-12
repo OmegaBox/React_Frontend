467957e6a169155252eae18ba47add001bff4f58
Seongkyun-Yu <ysungkyun@gmail.com>
2020-07-12 20:29:13 +0900

    feat: Make loading skeleton

---
+++ src/Components/Atoms/SkeletonTimeSelect.js
@@ -0,0 +1,71 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const SkeletonTimeSelect = () => {
+  return (
+    <div>
+      <ContentLoader
+        viewBox="0 0 470 70"
+        backgroundColor={"#d8d8d8"}
+        foregroundColor={"#eaeaea"}
+      >
+        <rect x="18" y="15" rx="5" ry="5" width="55" height="55" />
+        <rect x="85" y="27" rx="4" ry="4" width="280" height="10" />
+        <rect x="85" y="47" rx="3" ry="3" width="220" height="10" />
+        <rect x="410" y="22" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="37" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="52" rx="3" ry="3" width="40" height="10" />
+      </ContentLoader>
+      <ContentLoader
+        viewBox="0 0 470 70"
+        backgroundColor={"#d8d8d8"}
+        foregroundColor={"#eaeaea"}
+      >
+        <rect x="18" y="15" rx="5" ry="5" width="55" height="55" />
+        <rect x="85" y="27" rx="4" ry="4" width="280" height="10" />
+        <rect x="85" y="47" rx="3" ry="3" width="220" height="10" />
+        <rect x="410" y="22" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="37" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="52" rx="3" ry="3" width="40" height="10" />
+      </ContentLoader>
+      <ContentLoader
+        viewBox="0 0 470 70"
+        backgroundColor={"#d8d8d8"}
+        foregroundColor={"#eaeaea"}
+      >
+        <rect x="18" y="15" rx="5" ry="5" width="55" height="55" />
+        <rect x="85" y="27" rx="4" ry="4" width="280" height="10" />
+        <rect x="85" y="47" rx="3" ry="3" width="220" height="10" />
+        <rect x="410" y="22" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="37" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="52" rx="3" ry="3" width="40" height="10" />
+      </ContentLoader>
+      <ContentLoader
+        viewBox="0 0 470 70"
+        backgroundColor={"#d8d8d8"}
+        foregroundColor={"#eaeaea"}
+      >
+        <rect x="18" y="15" rx="5" ry="5" width="55" height="55" />
+        <rect x="85" y="27" rx="4" ry="4" width="280" height="10" />
+        <rect x="85" y="47" rx="3" ry="3" width="220" height="10" />
+        <rect x="410" y="22" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="37" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="52" rx="3" ry="3" width="40" height="10" />
+      </ContentLoader>
+      <ContentLoader
+        viewBox="0 0 470 70"
+        backgroundColor={"#d8d8d8"}
+        foregroundColor={"#eaeaea"}
+      >
+        <rect x="18" y="15" rx="5" ry="5" width="55" height="55" />
+        <rect x="85" y="27" rx="4" ry="4" width="280" height="10" />
+        <rect x="85" y="47" rx="3" ry="3" width="220" height="10" />
+        <rect x="410" y="22" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="37" rx="3" ry="3" width="40" height="10" />
+        <rect x="410" y="52" rx="3" ry="3" width="40" height="10" />
+      </ContentLoader>
+    </div>
+  );
+};
+
+export default SkeletonTimeSelect;
