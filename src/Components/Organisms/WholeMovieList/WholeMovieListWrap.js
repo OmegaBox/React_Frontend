a9449e8519e1503e856f7b2e7bff9bef8f60cf7b
hodeng28 <ghepd28@gmail.com>
2020-07-16 21:31:44 +0900

    fix: favorite page

--- src/Components/Organisms/WholeMovieList/WholeMovieListWrap.js
+++ src/Components/Organisms/WholeMovieList/WholeMovieListWrap.js
@@ -55,103 +55,103 @@ const WholeMovieListWrap = () => {
 
         </button>
       </div>
-      {isLoading ? (<SkeletonWholeMoviePage />) : (
-        <ul className="wholeMovieList">
-          {movies.map((movie, i) => {
-            let iconClassName = "icon";
-            switch (movie.grade) {
-              case "18+":
-                iconClassName += " ageGrade19Small";
-                break;
-              case "15+":
-                iconClassName += " ageGrade15Small";
-                break;
-              case "12+":
-                iconClassName += " ageGrade12Small";
-                break;
-              case "all":
-              default:
-                iconClassName += " ageGradeSmall";
-                break;
-            }
-            return (
-              <li className="wholeMovie" key={i}>
-                <Link to={"detail/" + movie.id}>
-                  <p className="mainRank">{movie.rank}</p>
-                  <img
-                    className="wholeMoviePoster"
-                    alt={movie.title}
-                    src={movie.poster}
-                  // style={{ backgroundColor: "gray" }}
-                  />
-                  <div className="wholeMovieInforWrap">
-                    <div className="wholeMovieSummary">
-                      <p>{movie.description}</p>
-                    </div>
-                    <div className="boxOfficeMovieScore">
-                      <div>
-                        <p>관람평</p>
-                        <strong>{(movie.average_point).toFixed(1)}</strong>
-                      </div>
+      {/* {isLoading ? (<SkeletonWholeMoviePage />) : ( */}
+      <ul className="wholeMovieList">
+        {movies.map((movie, i) => {
+          let iconClassName = "icon";
+          switch (movie.grade) {
+            case "18+":
+              iconClassName += " ageGrade19Small";
+              break;
+            case "15+":
+              iconClassName += " ageGrade15Small";
+              break;
+            case "12+":
+              iconClassName += " ageGrade12Small";
+              break;
+            case "all":
+            default:
+              iconClassName += " ageGradeSmall";
+              break;
+          }
+          return (
+            <li className="wholeMovie" key={i}>
+              <Link to={"detail/" + movie.id}>
+                <p className="mainRank">{movie.rank}</p>
+                <img
+                  className="wholeMoviePoster"
+                  alt={movie.title}
+                  src={movie.poster}
+                // style={{ backgroundColor: "gray" }}
+                />
+                <div className="wholeMovieInforWrap">
+                  <div className="wholeMovieSummary">
+                    <p>{movie.description}</p>
+                  </div>
+                  <div className="boxOfficeMovieScore">
+                    <div>
+                      <p>관람평</p>
+                      <strong>{(movie.average_point).toFixed(1)}</strong>
                     </div>
                   </div>
-                </Link>
-                <div className="movieListTitleWrap">
-                  <span className={iconClassName} />
-                  <span className="movieListTitle">{movie.name_kor}</span>
                 </div>
-                <div className="movieListRateandDay">
-                  <span className="movieListBookingRate">
-                    예매율{movie.reservation_rate}%
+              </Link>
+              <div className="movieListTitleWrap">
+                <span className={iconClassName} />
+                <span className="movieListTitle">{movie.name_kor}</span>
+              </div>
+              <div className="movieListRateandDay">
+                <span className="movieListBookingRate">
+                  예매율{movie.reservation_rate}%
                 </span>
-                  <span className="movieListOpeningDay">
-                    개봉일{movie.open_date}
-                  </span>
-                </div>
+                <span className="movieListOpeningDay">
+                  개봉일{movie.open_date}
+                </span>
+              </div>
 
-                <div className="wholeBtnWrap">
+              <div className="wholeBtnWrap">
+                <button
+                  className={[
+                    "wholeFavoriteBtn",
+                    "btn",
+                    "outLine",
+                    "lightGray",
+                    "small",
+                  ].join(" ")}
+                >
+                  <span className="icon favorite"></span>
+                  <span className="wholeFavoriteScore">
+                    {movie.acc_favorite}
+                  </span>
+                </button>
+                <Link to="/booking">
                   <button
+                    onClick={() =>
+                      dispatch(
+                        selectMovie({
+                          title: movie.name_kor,
+                          poster: movie.poster,
+                          id: movie.id,
+                        })
+                      )
+                    }
                     className={[
-                      "wholeFavoriteBtn",
+                      "wholeBookingBtn",
                       "btn",
-                      "outLine",
-                      "lightGray",
+                      "fill",
+                      "subLight",
                       "small",
                     ].join(" ")}
                   >
-                    <span className="icon favorite"></span>
-                    <span className="wholeFavoriteScore">
-                      {movie.acc_favorite}
-                    </span>
+                    예매
                   </button>
-                  <Link to="/booking">
-                    <button
-                      onClick={() =>
-                        dispatch(
-                          selectMovie({
-                            title: movie.name_kor,
-                            poster: movie.poster,
-                            id: movie.id,
-                          })
-                        )
-                      }
-                      className={[
-                        "wholeBookingBtn",
-                        "btn",
-                        "fill",
-                        "subLight",
-                        "small",
-                      ].join(" ")}
-                    >
-                      예매
-                  </button>
-                  </Link>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+                </Link>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      {/* )} */}
       <div className="wholeMovieListMore">
         <button type="button" className={["btn", "regular"].join(" ")}>
           더보기
