9ed4ce79a8396a8e1a6e17b20f5d12016270c2c9
Seongkyun-Yu <ysungkyun@gmail.com>
2020-07-25 23:21:29 +0900

    feat: Add theater map component

--- src/Components/Templates/TheaterInfo.js
+++ src/Components/Templates/TheaterInfo.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../Organisms/Header";
+import Footer from "../Organisms/Footer";
+import TheaterMap from "../Molecules/TeatherMap";
 
 const TheaterInfo = () => {
-  return <div>Component!</div>;
+  return (
+    <>
+      <Header />
+      <TheaterMap />
+      <Footer />
+    </>
+  );
 };
 
 export default TheaterInfo;
