77c9a3df4b0bbfa07751f94ac57dcf3eb26da58a
Mang-developer <chiwon618@gmail.com>
2020-07-29 23:10:44 +0900

    Fix: Removed setSize func in ticketVoucher
    필요없는 코드인 setSize 액션 생성함수 제거

--- src/Components/Molecules/ticketVoucher.js
+++ src/Components/Molecules/ticketVoucher.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { openModal, setSize } from "../../Reducer/modalReducer";
+import { openModal } from "../../Reducer/modalReducer";
 const BookingCancel = ({ classSet }) => {
   const dispatch = useDispatch();
   return (
@@ -8,7 +8,6 @@ const BookingCancel = ({ classSet }) => {
       type="button"
       className={classSet}
       onClick={() => {
-        dispatch(setSize(null, null));
         dispatch(openModal(`예매 취소하시겠습니까?`));
       }}
     >
