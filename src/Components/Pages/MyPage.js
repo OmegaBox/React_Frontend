5e35cee9e935a6c1d27d943f96fd318b3604614c
Seongkyun-Yu <ysungkyun@gmail.com>
2020-07-29 23:51:13 +0900

    fix: fix mypage login check bug 마이페이지 로그인 체크 버그 수정

--- src/Components/Pages/MyPage.js
+++ src/Components/Pages/MyPage.js
@@ -1,38 +1,18 @@
 import React, { useEffect } from "react";
 import MyMegaBox from "../Templates/MyMegaBox";
-import { useDispatch, useSelector } from "react-redux";
-import { checkLogin, getMemberProfile } from "../../Reducer/userInfoReducer";
-import { setOneBtn } from "../../Reducer/modalReducer";
-import ModalPortal from "../../Modules/ModalPortal";
-import PopupNotice from "../Molecules/PopupNotice";
+import { useDispatch } from "react-redux";
+import { getMemberProfile } from "../../Reducer/userInfoReducer";
 import "./style/mypage.scss";
 
 const MyPage = ({ history }) => {
   const dispatch = useDispatch();
-  const isLoginCheck = useSelector((state) => state.userInfo.isLogin);
 
-  if (!isLoginCheck) dispatch(setOneBtn());
   useEffect(() => {
     window.scrollTo(0, 0);
-    dispatch(checkLogin());
     dispatch(getMemberProfile());
   }, [dispatch]);
 
-  return (
-    <>
-      <MyMegaBox history={history} />
-      {!isLoginCheck && (
-        <ModalPortal>
-          <PopupNotice
-            text={"로그인이 필요한 페이지 입니다"}
-            onEvent={() => {
-              history.push("/memberlogin");
-            }}
-          />
-        </ModalPortal>
-      )}
-    </>
-  );
+  return <MyMegaBox history={history} />;
 };
 
 export default React.memo(MyPage);
